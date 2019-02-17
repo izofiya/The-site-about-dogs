c4cd02c7e7ced31d99b32a70ec7206ac97b7375c
izofiya <izofiya@mail.ru>
2019-02-17 22:42:28 +0300

    Updates

---
+++ precache-manifest.fe7f8377754f5251a10f731de92052a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6701b85c780ad41814b",
+    "url": "/react-app/static/css/main.04144122.chunk.css"
+  },
+  {
+    "revision": "d6701b85c780ad41814b",
+    "url": "/react-app/static/js/main.d6701b85.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/react-app/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "784ff0069516a535107c0848b247873f",
+    "url": "/react-app/index.html"
+  }
+];
